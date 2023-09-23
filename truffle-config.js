42af270d5bcf49e4101f7df159973a55428bb0a8
Annetta <67286396+annetta-zheng@users.noreply.github.com>
2023-09-22 22:24:33 -0400

    init

---
+++ truffle-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    networks: {
+      development: {
+        host: "127.0.0.1",
+        port: 7545, // Default Ganache port
+        network_id: "*", // Match any network id
+      },
+    },
+    // ...
+  };
+  
