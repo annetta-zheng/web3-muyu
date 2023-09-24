460676d1bf87aacb343a23b7e8c35e6651825c28
lydiazhou <zhouyuheng1231@gmail.com>
2023-09-23 23:04:38 -0400

    mint NFT

--- react/muyu/contracts/Muyu.sol
+++ react/muyu/contracts/Muyu.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Muyu {
+contract Muyu is ERC721, ERC721URIStorage {
     uint gongdeBalance;
 
-    constructor() {
+    constructor() ERC721("GongdeNFT", "GDE") {
         gongdeBalance = 0;
     }
 
@@ -18,4 +21,35 @@ contract Muyu {
         gongdeBalance += 1;
         console.log(gongdeBalance);
     }
+
+    // NFT related methods
+    // function safeMint(address to, uint256 tokenId, string memory uri)
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        // OnlyOwner
+    {
+        console.log("SafeMint");
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
 }
